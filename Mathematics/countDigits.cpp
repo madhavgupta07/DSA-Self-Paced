c9bca98aee707606ecb0d13f1e8412b531935ddf
Madhav Gupta <madhavgupta5555@gmail.com>
2022-10-22 22:20:21 +0530

    Solved countDigits problem

---
+++ Mathematics/countDigits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int countDigits(int num){
+    int ans = 0;
+    while (num > 0)
+    {
+        num = num /10;
+        ans++;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout<<"Enter Number: " ;
+    cin>>n;
+    cout<<"Total number of digits in "<<n<<" is: "<< countDigits(n);     
+    return 0;
+}
