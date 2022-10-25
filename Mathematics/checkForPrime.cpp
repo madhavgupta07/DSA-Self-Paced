3773b7d74801b9aad0c743f5fda5cabe60c38a20
Madhav Gupta <madhavgupta5555@gmail.com>
2022-10-25 10:30:13 +0530

    Added 3rd approach to solve checkForPrime problem

--- Mathematics/checkForPrime.cpp
+++ Mathematics/checkForPrime.cpp
@@ -18,15 +18,41 @@ using namespace std;
 // }
 
 // 2nd Approach
+// bool checkForPrime(int num)
+// {
+//     if (num == 1 || num <= 0)
+//     {
+//         return false;
+//     }
+//     for (int i = 2; i*i < num; i++)
+//     {
+//         if (num % i == 0)
+//             return false;
+//         else
+//             return true;
+//     }
+// }
+
+// 3rd Approach for large numbers
+
 bool checkForPrime(int num)
 {
     if (num == 1 || num <= 0)
     {
         return false;
     }
-    for (int i = 2; i*i < num; i++)
+    if (num == 2 || num == 3)
     {
-        if (num % i == 0)
+        return true;
+    }
+    if (num % 2 == 0 || num % 3 == 0)
+    {
+        return false;
+    }
+
+    for (int i = 5; i * i < num; i += 6)
+    {
+        if (num % i == 0 || num % (i + 2) == 0)
             return false;
         else
             return true;
@@ -35,12 +61,13 @@ bool checkForPrime(int num)
 
 int main()
 {
-    if (checkForPrime(1563))
+    if (checkForPrime(9) == false)
     {
-        cout<<"NO";
+        cout << "NO";
     }
-    else{
-        cout<<"YES";
+    else
+    {
+        cout << "YES";
     }
     return 0;
 }
