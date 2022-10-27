b25c038d045a4e5932ce10cbef5d7f1bc85266f1
Madhav Gupta <madhavgupta5555@gmail.com>
2022-10-27 22:17:30 +0530

    Updated printPrime problem

--- Mathematics/printPrime.cpp
+++ Mathematics/printPrime.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// 1st Approach
-// bool isPrime(int num)
-// {
-//     if (num <= 1)
-//     {
-//         return false;
-//     }
-//     for (int i = 2; i * i <= num; i++)
-//     {
-//         if (num % i == 0)
-//         {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+bool isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
-// void printPrime(int num)
-// {
-//     for (int i = 2; i <= num; i++)
-//     {
-//         if (isPrime(i))
-//         {
-//             cout << i << " ";
-//         }
-//     }
-// }
+void printPrime(int num)
+{
+    for (int i = 2; i <= num; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << " ";
+        }
+    }
+}
 
 // 2nd Approach
 
 
 int main()
 {
-    // printPrime(23);
+    printPrime(23);
     return 0;
 }
