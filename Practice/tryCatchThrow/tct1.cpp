fcf779c826274e5a0c76b9cf36860235a82233fd
Madhav Gupta <madhavgupta5555@gmail.com>
2022-11-10 08:25:01 +0530

    Basics of try, catch and throw

---
+++ Practice/tryCatchThrow/tct1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin>>x>>y;
+    try
+    {
+        if(y == 0){
+            throw 0;
+        }
+        cout<<"Result is: "<< x/y;
+    }
+    catch(int x)
+    {
+        cout<<"Divisor is zero"<<endl;
+    }
+         
+    return 0;
+}
