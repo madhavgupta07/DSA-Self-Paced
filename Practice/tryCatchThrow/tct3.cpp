4bcf731d545a503cf66cfeef0e7d51ff7cb1f1a4
Madhav Gupta <madhavgupta5555@gmail.com>
2022-11-10 21:04:22 +0530

    Average of n numbers

---
+++ Practice/tryCatchThrow/tct3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int average(int arr[], int n)
+{
+    if (n == 0)
+    {
+        throw string("Array is empty");
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    int avg;
+    avg = sum / n;
+    return avg;
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of items:" << "\n";
+    cin >> n;
+    int *arr = new int(n);
+    cout << "Enter " << n << " items" << endl;
+    for (int x = 0; x < n; x++)
+    {
+        cin >> arr[x];
+    }
+    try
+    {
+        int result = average(arr, n);
+        cout << (result);
+    }
+    catch (string &error)
+    {
+        cout << error;
+    }
+
+    return 0;
+}
